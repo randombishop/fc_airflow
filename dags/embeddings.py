754f83c2ec2d389b604886f6a6383d4d3728641e
randombishop <nabil.abdellaoui@expr-research.com>
2024-07-03 12:13:24 -0400

    embeds on

---
+++ dags/embeddings.py
@@ -0,0 +1,58 @@
+import airflow
+import datetime
+import os
+import sys
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
+# Import local utils
+dag_folder = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(dag_folder)
+from utils import exec_notebook
+
+
+def notebook(**kwargs):
+    ds = kwargs['ds']
+    sample_size = '100000'
+    params = {
+        "sample_day": ds,
+        "sample_size": sample_size,
+        'db_host': os.environ['PG_HOST'],
+        'db_database': os.environ['PG_REPLICATOR'],
+        'db_user': os.environ['PG_USERNAME'],
+        'db_password': os.environ['PG_PASSWORD']
+    }
+    task_name = 'embed'
+    description = 'Airflow job executing embeddings notebook'
+    gcs_notebook = 'gs://us-central1-airflow1-f888e353-bucket/notebooks/embeddings.ipynb'
+    instance_type = "n1-highcpu-32"
+    container_image_uri = "gcr.io/deeplearning-platform-release/base-cpu:latest"
+    kernel_spec = "python3"
+    exec_notebook(task_name, description, gcs_notebook, instance_type, container_image_uri, kernel_spec, params)
+
+
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(30),
+    'retries': 1,
+    'retry_delay': datetime.timedelta(hours=4)
+}
+
+with DAG(
+    'embeddings',
+    default_args=default_args,
+    description='Calculate embeddings for casts sample',
+    schedule_interval='0 6 * * *',
+    max_active_runs=1,
+    catchup=True,
+    dagrun_timeout=datetime.timedelta(hours=3),
+) as dag:
+
+    notebook_task = PythonOperator(
+        task_id='notebook',
+        python_callable=notebook,
+        provide_context=True
+    )
+
+    notebook_task
+
---
+++ dags/utils.py
@@ -0,0 +1,66 @@
+import datetime
+import logging
+import os
+import time
+from google.cloud import notebooks_v1
+from google import auth
+
+
+def exec_notebook(
+    task,
+    description,
+    gcs_notebook,
+    instance_type,
+    container_image_uri,
+    kernel_spec,
+    params):
+    logging.info("vertex_exec_notebook")
+    region = os.environ['EXECUTOR_REGION']
+    network = os.environ['EXECUTOR_NETWORK']
+    executor_folder = os.environ['EXECUTOR_FOLDER']
+    service_account = os.environ['EXECUTOR_SERVICE_ACCOUNT']
+    _, project = auth.default()
+    parent = "projects/" + project + "/locations/" + region
+    execution_id = task+'_'+str(datetime.datetime.now()).replace(' ','').replace('-','').replace(':','')[:12]
+    output_notebook = executor_folder + '/outputs/' + execution_id + '.ipynb'
+    params_s = ','.join([k+'='+v for k,v in params.items()])
+    execution_config = {
+        "execution_template": {
+            "scale_tier": "CUSTOM",
+            "master_type": instance_type,
+            "input_notebook_file": gcs_notebook,
+            "output_notebook_folder": executor_folder,
+            "container_image_uri": container_image_uri,
+            "parameters": params_s,
+            "service_account": service_account,
+            "kernel_spec": kernel_spec,
+            "vertex_ai_parameters": {
+                "network": network
+            }
+        },
+        "display_name": task,
+        "description": description,
+        "output_notebook_file": output_notebook
+    }
+    logging.info('execution_config='+str(execution_config))
+    client = notebooks_v1.NotebookServiceClient()
+    logging.info("notebooks_v1 client="+str(client))
+    request = notebooks_v1.CreateExecutionRequest(
+        parent=parent,
+        execution_id=execution_id,
+        execution=execution_config
+    )
+    execution = client.create_execution(request=request)
+    response = execution.result()
+    execution_name = response.name
+    logging.info('execution_name='+execution_name)
+    response = client.get_execution(name=execution_name)
+    check_every_sec = 120
+    while response.state in [notebooks_v1.Execution.State.QUEUED, notebooks_v1.Execution.State.PREPARING, notebooks_v1.Execution.State.RUNNING]:
+        logging.info('current state='+str(response.state))
+        time.sleep(check_every_sec)
+        response = client.get_execution(name=execution_name)
+    logging.info('final state='+str(response.state))
+    if response.state != notebooks_v1.Execution.State.SUCCEEDED:
+        raise Exception(f"Notebook execution failed")
+    
