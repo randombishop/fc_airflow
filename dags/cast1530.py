58467e12b364360de7a7fae375432a99b6caa5a9
randombishop <nabil.abdellaoui@expr-research.com>
2024-07-15 20:06:49 -0400

    cast1530

---
+++ dags/cast1530.py
@@ -0,0 +1,35 @@
+import airflow
+from airflow import DAG
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from datetime import timedelta
+
+
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=30)
+}
+
+with DAG(
+    'cast1530',
+    default_args=default_args,
+    schedule_interval='30 15 * * *',
+    max_active_runs=1,
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=30),
+) as dag:
+
+    t1 = SSHOperator(
+        task_id='t1',
+        ssh_conn_id='ssh_caster',
+        command='/home/na/.bun/bin/bun fc_caster/app/index.ts digest "{{ ds }}" "c_crypto"',
+        cmd_timeout=900,
+        get_pty=True)
+    
+    t1
+    
+    
+
+
+
+
