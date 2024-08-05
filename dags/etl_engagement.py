5e04ba557402edb46496f57473d6e439b3956e9c
randombishop <nabil.abdellaoui@expr-research.com>
2024-08-04 20:51:59 -0400

    etl engagement

---
+++ dags/etl_engagement.py
@@ -0,0 +1,59 @@
+import datetime
+import airflow
+from airflow import DAG
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+
+
+
+
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(100),
+    'retries': 1,
+    'retry_delay': datetime.timedelta(hours=1)
+}
+
+
+with DAG(
+    'etl_engagement',
+    default_args=default_args,
+    description='Hourly engagement stats',
+    schedule_interval='0 * * * *',
+    max_active_runs=1,
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(hours=1)
+) as dag:
+
+    etleng_1h = PostgresToGCSOperator(
+        task_id="etleng_1h",
+        postgres_conn_id='pg_replicator',
+        sql='sql/engagement_1h.sql',
+        bucket='dsart_nearline1',
+        filename='pipelines/etl_engagement/snapshot_1h/{{ (execution_date - macros.timedelta(hours=1)).strftime("%Y-%m-%d-%H") }}.csv',
+        export_format="csv",
+        gzip=False
+    )
+
+    etleng_12h = PostgresToGCSOperator(
+        task_id="etleng_12h",
+        postgres_conn_id='pg_replicator',
+        sql='sql/engagement_12h.sql',
+        bucket='dsart_nearline1',
+        filename='pipelines/etl_engagement/snapshot_12h/{{ (execution_date - macros.timedelta(hours=12)).strftime("%Y-%m-%d-%H") }}.csv',
+        export_format="csv",
+        gzip=False
+    )
+
+    etleng_36h = PostgresToGCSOperator(
+        task_id="etleng_36h",
+        postgres_conn_id='pg_replicator',
+        sql='sql/engagement_36h.sql',
+        bucket='dsart_nearline1',
+        filename='pipelines/etl_engagement/snapshot_36h/{{  (execution_date - macros.timedelta(hours=36)).strftime("%Y-%m-%d-%H") }}.csv',
+        export_format="csv",
+        gzip=False
+    )
+
+    etleng_1h >> etleng_12h >> etleng_36h
+
+
+
---
+++ dags/sql/engagement_12h.sql
@@ -0,0 +1,28 @@
+SELECT id, 
+	
+	timestamp::date AS day,
+    EXTRACT(HOUR FROM timestamp) AS hour,
+	encode(casts.hash, 'hex') as cast_hash,
+	deleted_at,
+	
+	(select count(distinct fid) from messages 
+	 where body->'target'->>'hash'=('0x'||encode(casts.hash, 'hex'))
+	 and body->>'type'='1'
+     and timestamp<(casts.timestamp+INTERVAL '12 hours')) as num_like,
+
+	(select count(distinct fid) from messages 
+	 where body->'target'->>'hash'=('0x'||encode(casts.hash, 'hex'))
+	 and body->>'type'='2'
+     and timestamp<(casts.timestamp+INTERVAL '12 hours')) as num_recast,
+
+	(select count(distinct fid) from messages 
+	 where body->'parent'->>'hash'=('0x'||encode(casts.hash, 'hex'))
+	 and type=1
+     and timestamp<(casts.timestamp+INTERVAL '12 hours')) as num_reply
+	
+FROM public.casts
+
+WHERE 
+timestamp>'{{ execution_date - macros.timedelta(hours=12) }}'
+AND timestamp<'{{ execution_date - macros.timedelta(hours=11) }}'
+AND (deleted_at is not NULL or num_like>0 or num_recast>0 or num_reply>0)
