88d84ce6e4eb2e52235b4d4ed41f9e008015f1bb
randombishop <nabil.abdellaoui@expr-research.com>
2024-08-15 13:30:18 -0400

    publish channel digest

--- dags/bot_activity.py
+++ dags/bot_activity.py
@@ -2,7 +2,7 @@ import datetime
 import airflow
 from airflow import DAG
 from airflow.operators.postgres_operator import PostgresOperator
-
+from airflow.providers.ssh.operators.ssh import SSHOperator
 
 
 default_args = {
@@ -22,11 +22,20 @@ with DAG(
     dagrun_timeout=datetime.timedelta(hours=3)
 ) as dag:
 
-    update_cd = PostgresOperator(
-		task_id='update_ca',
+    update1 = PostgresOperator(
+		task_id='update1',
 		postgres_conn_id='pg_replicator',
 		sql='sql/update_channels_digest.sql')
+    update1
     
-    update_cd
+    publish1 = SSHOperator(
+        task_id='publish1',
+        ssh_conn_id='ssh_worker',
+        command='/home/na/channel_digest.sh',
+        cmd_timeout=300,
+        get_pty=True)
+    publish1
+    
+    update1 >> publish1
 
     
