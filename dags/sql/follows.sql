998705d086e1a12c21a5f2d43c18f5702ef07b1c
randombishop <nabil.abdellaoui@expr-research.com>
2024-08-16 17:16:54 -0400

    BIGINT

--- dags/sql/follows.sql
+++ dags/sql/follows.sql
@@ -7,7 +7,7 @@ FROM public.messages
 WHERE timestamp>'{{ execution_date }}'
 AND timestamp<'{{ execution_date + macros.timedelta(hours=1) }}'
 AND type in (5, 6)
-AND (body->>'targetFid')::BIGINTEGER > 0
-AND (body->>'targetFid')::BIGINTEGER < 100000000
+AND (body->>'targetFid')::BIGINT > 0
+AND (body->>'targetFid')::BIGINT < 100000000
 GROUP BY fid_follower, fid_followed
 
