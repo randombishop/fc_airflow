e220c30f3424cc9d2063483ce3d6a8dc67aeac28
randombishop <nabil.abdellaoui@expr-research.com>
2024-08-16 11:46:46 -0400

    social graph

---
+++ dags/social_graph.py
@@ -0,0 +1,87 @@
+import datetime
+import airflow
+from airflow import DAG
+from airflow.operators.sql import SQLCheckOperator
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+
+
+
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(1),
+    'retries': 1,
+    'retry_delay': datetime.timedelta(hours=1)
+}
+
+
+with DAG(
+    'social_graph',
+    default_args=default_args,
+    description='Process social graph and push to Big Query',
+    schedule_interval='0 * * * *',
+    max_active_runs=1,
+    catchup=True,
+    dagrun_timeout=datetime.timedelta(hours=3)
+) as dag:
+
+    check = SQLCheckOperator(
+        task_id='check',
+        conn_id='pg_replicator',
+		sql='sql/check_casts.sql')
+    
+    check
+
+    users_filename = 'pipelines/social_graph/user_names/{{ execution_date.strftime("%Y-%m-%d-%H") }}_users.csv'
+    users1 = PostgresToGCSOperator(
+        task_id="users1",
+        postgres_conn_id='pg_replicator',
+        sql='sql/user_names.sql',
+        bucket='dsart_nearline1',
+        filename=users_filename,
+        export_format="csv",
+        gzip=False
+    )
+
+    users2 = GCSToBigQueryOperator(
+        task_id='users2',
+        bucket='dsart_nearline1',
+        source_objects=[users_filename],
+        destination_project_dataset_table='deep-mark-425321-r7.dsart_farcaster.tmp_user_names',
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
+        source_format='CSV',
+        autodetect=True
+    )
+    
+    users3 = BigQueryInsertJobOperator(
+        task_id='users3',
+        configuration={
+            'query': {
+                'query': """
+                    UPDATE `deep-mark-425321-r7.dsart_farcaster.fid_username` AS t
+                    SET t.last_cast = IFNULL(s.last_cast, t.last_cast),
+                        t.num_casts = t.num_casts + s.num_casts,
+                        t.username = IFNULL(s.username, t.username)
+                    FROM `deep-mark-425321-r7.dsart_farcaster.tmp_user_names` AS s
+                    WHERE t.fid = s.fid ;
+                    
+                    INSERT INTO `deep-mark-425321-r7.dsart_farcaster.fid_username`
+                    SELECT s.*
+                    FROM `deep-mark-425321-r7.dsart_farcaster.tmp_user_names` AS s
+                    LEFT JOIN `deep-mark-425321-r7.dsart_farcaster.fid_username` AS t
+                    ON s.fid = t.fid
+                    WHERE t.fid IS NULL;
+                """,
+                'useLegacySql': False,
+            }
+        }
+    )
+    
+    check 
+    
+    check >> users1 >> users2  >> users3
+
+    
+
--- dags/sql/user_names.sql
+++ dags/sql/user_names.sql
@@ -1,14 +1,19 @@
 SELECT 
-	fid, 
-	min(timestamp) as first_cast,
-	max(timestamp) as last_cast,
-	count(*) as num_casts,
-	(SELECT DISTINCT ON (fid)
-	    body->>'value'
-        FROM messages
-        WHERE fid=casts.fid 
-        AND type = 11 
-        AND body->>'type' = '6'
-	    ORDER BY fid, timestamp DESC) as username
-FROM public.casts
-GROUP BY fid
+	main.fid, 
+	min(main.timestamp) FILTER (WHERE type = 1) as first_cast,
+	max(main.timestamp) FILTER (WHERE type = 1) as last_cast,
+	COUNT(main.id) FILTER (WHERE type = 1) as num_casts,
+	(
+		SELECT body->>'value' 
+		FROM public.messages AS sub
+		WHERE sub.fid = main.fid 
+			AND sub.type = 11 
+			AND sub.body->>'type' = '6'
+		ORDER BY sub.timestamp DESC
+		LIMIT 1
+	) AS user_name
+FROM public.messages as main
+WHERE main.timestamp>'{{ execution_date }}'
+AND main.timestamp<'{{ execution_date + macros.timedelta(hours=1) }}'
+GROUP BY main.fid
+
