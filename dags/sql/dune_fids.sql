cddffa0c0b52b0141c7639dec929ed8a9a6e335c
randombishop <nabil.abdellaoui@expr-research.com>
2024-10-30 16:07:31 -0400

    dune prefs

--- dags/sql/dune_fids.sql
+++ dags/sql/dune_fids.sql
@@ -203,7 +203,80 @@ WHERE
     rank <= 25
 GROUP BY
     fid
+),
+
+prefs_rows AS (
+SELECT
+fid,
+q_clear,q_audience,q_info,q_easy,q_verifiable,q_personal,q_funny,q_meme_ref,q_emo_res,q_happiness,q_curiosity,q_aggressivity,q_surprise,
+q_interesting_ask,q_call_action,c_arts,c_business,c_crypto,c_culture,c_misc,c_money,c_na,c_nature,c_politics,c_sports,c_tech_science,
+4 as weight
+FROM dune.dsart.casts_features
+WHERE timestamp>date_add('day', -30, current_date)
+AND timestamp<current_date
+AND (c_arts+c_business+c_crypto+c_culture+c_misc+c_money+c_na+c_nature+c_politics+c_sports+c_tech_science)>0  
+UNION
+SELECT 
+r.fid,
+q_clear,q_audience,q_info,q_easy,q_verifiable,q_personal,q_funny,q_meme_ref,q_emo_res,q_happiness,q_curiosity,q_aggressivity,q_surprise,
+q_interesting_ask,q_call_action,c_arts,c_business,c_crypto,c_culture,c_misc,c_money,c_na,c_nature,c_politics,c_sports,c_tech_science,
+(CASE WHEN reaction_type=1 THEN 2 ELSE 3 END) as weight
+FROM dune.neynar.dataset_farcaster_reactions r
+INNER JOIN dune.dsart.casts_features cf ON cf.hash=r.target_hash
+WHERE r.timestamp>date_add('day', -30, current_date)
+AND r.timestamp<current_date
+AND (c_arts+c_business+c_crypto+c_culture+c_misc+c_money+c_na+c_nature+c_politics+c_sports+c_tech_science)>0
+UNION
+SELECT 
+t1.fid,
+t2.q_clear,t2.q_audience,t2.q_info,t2.q_easy,t2.q_verifiable,t2.q_personal,t2.q_funny,t2.q_meme_ref,t2.q_emo_res,t2.q_happiness,t2.q_curiosity,
+t2.q_aggressivity,t2.q_surprise,t2.q_interesting_ask,t2.q_call_action,t2.c_arts,t2.c_business,t2.c_crypto,t2.c_culture,t2.c_misc,t2.c_money,
+t2.c_na,t2.c_nature,t2.c_politics,t2.c_sports,t2.c_tech_science,
+1 as weight
+FROM dune.dsart.casts_features t1 
+INNER JOIN dune.dsart.casts_features t2 ON t2.hash=t1.parent_hash 
+WHERE t1.timestamp>date_add('day', -30, current_date)
+AND t1.timestamp<current_date
+AND (t2.c_arts+t2.c_business+t2.c_crypto+t2.c_culture+t2.c_misc+t2.c_money+t2.c_na+t2.c_nature+t2.c_politics+t2.c_sports+t2.c_tech_science)>0  
+),
+
+t_prefs AS (
+SELECT
+    fid prefs_fid,
+    SUM(weight) AS prefs_weight,
+    SUM(weight * q_clear) / SUM(weight) AS prefs_q_clear,
+    SUM(weight * q_audience) / SUM(weight) AS prefs_q_audience,
+    SUM(weight * q_info) / SUM(weight) AS prefs_q_info,
+    SUM(weight * q_easy) / SUM(weight) AS prefs_q_easy,
+    SUM(weight * q_verifiable) / SUM(weight) AS prefs_q_verifiable,
+    SUM(weight * q_personal) / SUM(weight) AS prefs_q_personal,
+    SUM(weight * q_funny) / SUM(weight) AS prefs_q_funny,
+    SUM(weight * q_meme_ref) / SUM(weight) AS prefs_q_meme_ref,
+    SUM(weight * q_emo_res) / SUM(weight) AS prefs_q_emo_res,
+    SUM(weight * q_happiness) / SUM(weight) AS prefs_q_happiness,
+    SUM(weight * q_curiosity) / SUM(weight) AS prefs_q_curiosity,
+    SUM(weight * q_aggressivity) / SUM(weight) AS prefs_q_aggressivity,
+    SUM(weight * q_surprise) / SUM(weight) AS prefs_q_surprise,
+    SUM(weight * q_interesting_ask) / SUM(weight) AS prefs_q_interesting_ask,
+    SUM(weight * q_call_action) / SUM(weight) AS prefs_q_call_action,
+    SUM(weight * c_arts) / SUM(weight) AS prefs_c_arts,
+    SUM(weight * c_business) / SUM(weight) AS prefs_c_business,
+    SUM(weight * c_crypto) / SUM(weight) AS prefs_c_crypto,
+    SUM(weight * c_culture) / SUM(weight) AS prefs_c_culture,
+    SUM(weight * c_misc) / SUM(weight) AS prefs_c_misc,
+    SUM(weight * c_money) / SUM(weight) AS prefs_c_money,
+    SUM(weight * c_na) / SUM(weight) AS prefs_c_na,
+    SUM(weight * c_nature) / SUM(weight) AS prefs_c_nature,
+    SUM(weight * c_politics) / SUM(weight) AS prefs_c_politics,
+    SUM(weight * c_sports) / SUM(weight) AS prefs_c_sports,
+    SUM(weight * c_tech_science) / SUM(weight) AS prefs_c_tech_science
+FROM
+    prefs_rows
+GROUP BY
+    fid
 )
+
+
 
 
 
@@ -214,6 +287,7 @@ t_casts_30d.*,
 t_channels.*,
 t_react_out.*,
 t_react_in.*,
+t_prefs.*,
 t_lang.*,
 t_keywords.*
 FROM t_users 
@@ -222,6 +296,7 @@ LEFT JOIN t_casts_30d ON t_casts_30d.casts_30d_fid=t_users.fid
 LEFT JOIN t_channels ON t_channels.channels_fid=t_users.fid
 LEFT JOIN t_react_out ON t_react_out.react_out_fid=t_users.fid
 LEFT JOIN t_react_in ON t_react_in.react_in_fid=t_users.fid
+LEFT JOIN t_prefs ON t_prefs.prefs_fid=t_users.fid
 LEFT JOIN t_lang ON t_lang.lang_fid=t_users.fid
 LEFT JOIN t_keywords ON t_keywords.keywords_fid=t_users.fid
 ORDER BY fid
